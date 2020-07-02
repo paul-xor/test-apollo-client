3921786441163ec336d6a0bd245545eac25607cb
Pasha <palexsmol@gmail.com>
2020-07-01 19:49:58 -0700

    add query

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import ApolloBoost, {gql} from 'apollo-boost'
+import ApolloBoost, { gql } from 'apollo-boost'
 
 const client = new ApolloBoost({
-    uri: 'http//localhost:9090'
+    uri: 'http://localhost:9090/graphql'
 })
 
-const getShipments = gql `
-    query {
-        shipments {
+
+const getShipments = gql`
+query listShipments($skip:Int, $limit: Int) {
+    shipments(skip:$skip, limit: $limit){
+            count
+            shipments{
             id
-            
+            shipmentId
+            customer{
+                customerId
+                useName
+                legalName
+            }
+            packages{
+                packageType
+                pcs
+                commodity
+                heigth
+                width
+                lenght
+                dg
+            }
+            }
         }
     }
 `
 
 client.query({
-    query
+    query: getShipments
+}).then((response) => {
+    console.log(response.data);
 })
